9ed411a5588dc582a3c6c537fb47d00cb2c2f2a2
Corey Baswell <corey.baswell@tekfive.com>
2024-02-18 14:13:03 -0600

    Backend initial setup.

---
+++ backend/config/initializers/config.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+Config.setup do |config|
+  config.const_name = 'Settings'
+  config.use_env = true
+  config.env_prefix = 'Settings'
+end
---
+++ backend/config/initializers/filter_parameter_logging.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+# Be sure to restart your server when you modify this file.
+
+# Configure sensitive parameters which will be filtered from the log file.
+ALLOWLIST = %w[
+  controller
+  action
+  id
+  from_date
+  to_date
+  qqtotalfilesize
+  type
+  folder_id
+  startDate
+  endDate
+  included
+  page
+  useCache
+  number
+  size
+  sort
+  showCompleted
+  excludeProvidedMessage
+  document_id
+  document_type
+  category
+  cookie_id
+  reply_id
+  ids
+  code
+  endpoint_sid
+  message_id
+  os_name
+  filter
+].freeze
+Rails.application.config.filter_parameters = [lambda do |k, v|
+  v.replace('FILTERED') if v.is_a?(String) && ALLOWLIST.exclude?(k)
+end]
