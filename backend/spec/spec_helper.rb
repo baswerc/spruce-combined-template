e7f3d7302e80ffa77bd4e8b156ed5a7c3966eb0c
Corey Baswell <corey.baswell@tekfive.com>
2024-02-20 18:50:51 -0600

    Backend setup.

--- backend/spec/spec_helper.rb
+++ backend/spec/spec_helper.rb
@@ -4,6 +4,8 @@ require 'simplecov'
 require 'vcr'
 require 'factory_bot_rails'
 
+SimpleCov.start
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
