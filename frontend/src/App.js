712e61947e850fcb425b1973d7d821fa4a631124
Corey Baswell <corey.baswell@tekfive.com>
2024-03-08 09:02:27 -0600

    Backend setup.

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import "@department-of-veterans-affairs/component-library/dist/main.css"
+import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { applyPolyfills, defineCustomElements } from '@department-of-veterans-affairs/component-library';
+import Homepage from './Homepage';
+import Second from './Second';
+
+function App() {
+
+    useEffect(() => {
+        // Apply polyfills and define all custom elements provided by the package
+        applyPolyfills().then(() => {
+            defineCustomElements(window);
+        });
+    }, []);
+
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+            <Route exact path="/">
+                <Homepage />
+            </Route>
+            <Route exact path="/second">
+                <Second />
+            </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Second.js
@@ -0,0 +1,22 @@
+function Second() {
+    return (
+        <va-card background>
+            <va-breadcrumbs
+                label="Breadcrumb"
+            >
+                <a href="#home">
+                    VA.gov home
+                </a>
+                <a href="#one">
+                    Level one
+                </a>
+                <a href="#two">
+                    Level two
+                </a>
+            </va-breadcrumbs>
+            <p>Example card content</p>
+        </va-card>
+    )
+}
+
+export default Second;
